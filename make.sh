c79dfb464072d9120a81098d8e0bff99e3f5ffbf
Owen Rodley <Strigoides@gmail.com>
2013-06-27 17:40:17 +1200

    Add `clean' argument to make.sh to delete jso files

--- make.sh
+++ make.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sbcl --load 'jscl.lisp' --eval '(jscl:bootstrap)' --eval '(quit)'
+if [ $1 = clean ]; then
+	rm -f *.jso src/*.jso tests/*.jso
+else
+	sbcl --load 'jscl.lisp' --eval '(jscl:bootstrap)' --eval '(quit)'
+fi
